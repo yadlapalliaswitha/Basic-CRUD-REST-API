048cbb28822e375c071c14ed020664861a745a96
yadlapalliaswitha <yadlapalliaswitha@gmail.com>
2025-07-09 23:04:34 -0400

    Added code for open api doc, swagger and method level security

--- src/main/java/com/example/emsapi/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.emsapi.repository;
-
-import com.example.emsapi.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
